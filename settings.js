560290663d6b0aabdf7640b274b6425c5ed00b3a
Sithujaya01 <88407351+Sithujaya01@users.noreply.github.com>
2022-04-21 05:26:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,46 +12,46 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.autoketik = true //status auto mengetik (auto typing)
+global.available = true //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "JF_BOT" //namabot kalian
+global.ownername= "Jayarathne_Technical" //nama kalian
+global.myweb ="https://chat.whatsapp.com/JE6kSDzCnBr1xDJ9MVxaZ1" //bebas asal jan hapus
+global.youtube = "https://chat.whatsapp.com/JE6kSDzCnBr1xDJ9MVxaZ1" //bebas asal jan hapus
+global.github = "https://chat.whatsapp.com/JE6kSDzCnBr1xDJ9MVxaZ1" //bebas
+global.email = "jayarathnefuneral@gmail.com" //bebas
+global.region = "SriLanka" //bebas
+global.ownernomer = "94778115292" // nomor wa kalian
+global.ownernomerr = "+94778115292" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["94778115292","94777611095","94702978512"] //ganti agar fitur owner bisa di gunakan
+global.packname = 'JF_BOT' //sticker wm ubah
+global.author = 'JAYARATHNE_TECHNICAL' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','#','sik','Jf']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+    sukses: '🍂 Done. Nice',
+    admin: 'This Command is only for ```Jayarathne``` 🚫🙂',
+    botAdmin: 'I`m not an ~Admin~ :(',
+    owner: 'This Command is only for ```Jayarathne``` 🚫🙂',
+    group: '👥 This feature can only be used in Group chat 🚫 ;)',
+    private: 'The feature can only be used in private chat 🚫 ;)',
+	bot: 'Bot Number Special Features :)',
+    errtoimg: 'Not Supported ;(',
+    wait: 'Processing... :)',
+	lockCmd: 'Features Not Activated By JAYARATHNE ;(',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
